f110bc0c46166d30c96a18d4b212ba286ebf008c
Oretugaadewale <Oretugafolorunso@gmail.com>
2023-04-20 16:35:08 +0100

    adding files

---
+++ src/Review.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+// because it a review page we dont need to iterate/map
+const Review = () => {
+  //we set the state value to 0 cos we wanna start working on the data one by and array is starting from 0
+  const [index, setIndex] = useState(0);
+  //we set people to index because we the data to start from index zero one by one
+  const { name, job, image, text } = people[index];
+
+  // to check the index or no in the array is not more or lower than so that it wont give error
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    // return no if no if no is nor grater or lower
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+  // This to set the random number of the review so when clicked it wont show 1 page twice
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me{" "}
+      </button>
+    </article>
+  );
+};
+
+export default Review;
